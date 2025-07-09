21510e6ffd425c19a1ac0847dc22c6d8b9e2410e
IsaacDev14 <mwitiisaac14@gmail.com>
2025-07-09 08:49:46 +0300

    Set up simulation environment with timing and variables

--- simulation.py
+++ simulation.py
@@ -9,3 +9,18 @@ class Customer:
         self.start_time = None
         self.wait_time = None
     
+# Simulation Time Loop
+    def run_simulation():
+        simulation_time = 60
+        max_arrival_interval = 5
+        max_service_time = 10
+
+        time = 0
+        next_arrival = random.randint(1, max_arrival_interval) 
+
+        queue = []
+        Customer = []
+        id_counter = 1
+
+        print("== Simulations Begins ===")
+
