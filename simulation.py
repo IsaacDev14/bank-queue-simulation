b92bb8f022aa1b2d0d56008f5a950060d998bec7
IsaacDev14 <mwitiisaac14@gmail.com>
2025-07-09 08:39:14 +0300

    Add Customer class to model queue data

--- simulation.py
+++ simulation.py
@@ -1,1 +1,11 @@
- 
+import random
+
+# Define a class to represent each customer in the simulation
+class Customer:
+    def __init__(self, id, arrival_time, service_time):
+        self.id = id
+        self.arrival_time = arrival_time
+        self.serrvice_time = service_time
+        self.start_time = None
+        self.wait_time = None
+    
